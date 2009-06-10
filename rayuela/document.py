c55682d08d3368925679ad041998b05ffced1f4c
Manuel Eduardo Ospina Sarmiento <mospina@mospina.csb>
2009-06-10 16:04:25 +1000

    document.py in progress. I need a new loader class for the v02 format.

--- rayuela/document.py
+++ rayuela/document.py
@@ -142,150 +142,138 @@ class Location:
 
         return profile
 
-#################################################
-# 
-# Buffer to XML
-#
-#################################################
-import re
+class Header:
 
-(INIT,
- TITLE,
- SECTION,
- ENDSECTION,
- HEADER,
- PARA,
- BREAK
-) = range(7)
+    def __init__(self):
+        self.title = ''
+        self.author = ''
+        self.language = ''
+        self.summary = ''
+        self.notes = ''
 
-(STARTTAG, 
- ENDTAG, 
- TEXT) = range(1,4)
+    def to_string(self):
+        # Open tag
+        result = '<head>\n'
+        result += '<title>%s</title>\n' % self.title
+        # Adding optional elements
+        if self.author:
+            result += '<author>%s</author>\n' % self.author
+        if self.language:
+            result += '<language>%s</language>\n' % self.language
+        if self.summary:
+            result += '<summary>\n%s\n</summary>\n' % self.summary
+        if self.notes:
+            result += '<notes>\n%s\n</notes>\n' % self.notes
+        # Close tag
+        result += '</head>\n'
+        return result
 
-seudo_xml = re.compile(r"""<(\w+).+?>       # 1. starttag
-                           |</(\w+)>        # 2. endtag
-                           |([^<>\n]+)      # 3. text strings
-                        """, re.VERBOSE)
-
-
-
-def scanner(txt):
-    result_list = []
-    scan = seudo_xml.scanner(txt)
-    while True:
-        m = scan.match()
-        if not m:
-            break
-        result_list.append([m.lastindex, m.group(m.lastindex)])
-    return result_list[0]
-
-def state_machine(document):
-    txt = ''
-    STATE = INIT
-    sections = False
-    lines = document.split('\n')
-    for line in lines:
-        if not line:
-            if STATE == PARA:
-                txt += '</p>\n'
-                STATE = BREAK
-            continue
-        token = scanner(line)
-        if STATE == INIT:
-            if token[0] == STARTTAG and token[1] == 'title':
-                txt += line
-                txt += '\n'
-                STATE = TITLE
-                continue
-            else:
-                assert False
-        if STATE == TITLE:
-            if token[0] == STARTTAG and token[1] == 'section':
-                txt += line
-                txt += '\n'
-                STATE = SECTION
-                sections = True
-                continue
-            elif token[0] == TEXT:
-                txt += '<p>\n'
-                txt += line
-                txt += '\n'
-                STATE = PARA
-                continue
-            else:
-                assert False
-        if STATE == SECTION:
-            if token[0] == STARTTAG and token[1] == 'title':
-                txt += line
-                txt += '\n'
-                STATE = HEADER
-                continue
-            else:
-                assert False
-        if STATE == HEADER:
-            if token[0] == ENDTAG and token[1] == 'section':
-                txt += line
-                txt += '\n'
-                STATE = ENDSECTION
-                continue
-            elif token[0] == TEXT:
-                txt += '<p>\n'
-                txt += line
-                txt += '\n'
-                STATE = PARA
-                continue
-            else:
-                assert False
-        if STATE == ENDSECTION:
-            if token[0] == STARTTAG and token[1] == 'section':
-                txt += line
-                txt += '\n'
-                STATE = SECTION
-                continue
-            elif token[0] == TEXT:
-                txt += '<p>\n'
-                txt += line
-                txt += '\n'
-                STATE = PARA
-                continue
-            else:
-                assert False
-        if STATE == PARA:
-            if token[0] == ENDTAG and token[1] == 'section':
-                txt += line
-                txt += '\n'
-                STATE = ENDSECTION
-                continue
-            elif token[0] == TEXT:
-                txt += '<br />\n'
-                txt += line
-                txt += '\n'
-                STATE = PARA
-                continue
-            else:
-                assert False
+class Document(list):
     
-        if STATE == BREAK:
-            if token[0] == ENDTAG and token[1] == 'section':
-                txt += line
-                txt += '\n'
-                STATE = ENDSECTION
-                continue
-            elif token[0] == TEXT:
-                txt += '<p>\n'
-                txt += line
-                txt += '\n'
-                STATE = PARA
-                continue
-            else:
-                assert False
+    def __init__(self, page, buffer):
+        list.__init__(self)
 
-    if sections and STATE != ENDSECTION:
-        txt += '</section>\n'
+        self.page = page
+        self.buffer = buffer
+        self.head = Header()
+        self.character = []
+        self.location = []
+        self.filename = ''
+        self.filepath = ''
 
-    return txt
-#################################################
+    def load_file(self, filename):
+        assert os.path.isfile(filename)
+        self.filepath, self.filename = os.path.split(filename)
+        # [TODO] 
+        # priority: high
+        # There are no scanner for v0.2!
+        # Use the python default xml library if possible.
+        import scanner
+        fh = open(filename)
+        scanner.scan(fh.read(), Loader(self))
+        #.
+        fh.close()
+    
+    def dump_file(self, filename=''):
+        start = self.buffer.get_start_iter()
+        end = self.buffer.get_end_iter()
+        txt = self.buffer.get_text(start, end)
 
+        if not filename:
+            filename = os.path.join(self.filepath, self.filename)
+        else:
+            self.filepath, self.filename = os.path.split(filename)
+        
+        # Open tag:
+        document = '<rayuela>\n' # lang="%s"> % self.lang
+
+        # sections:
+        sections = "<toc>\n"
+        for section in self:
+            sections += section.to_string()
+        sections += "</toc>\n" 
+
+        document += sections
+        
+        # characters:
+        characters = "<characters>\n"
+        for n in self.character:
+            characters += n.to_string()
+        characters += "</characters>\n" 
+
+        document += characters
+
+        # locations:
+        locations = "<locations>\n"
+        for n in self.location:
+            locations += n.to_string()
+        locations += "</locations>\n" 
+
+        document += locations
+
+        # Add manuscript:
+        manuscript = '<manuscript>\n'
+
+        # Add head:
+        manuscript += self.head.to_string()
+
+        # Add body:
+        body = '<body>\n%s\n</body>\n' % txt.strip()
+        
+        manuscript += body
+        manuscript += '</manuscript>\n'
+
+        document += manuscript
+
+        # Close tag:
+        document += '</rayuela>'
+
+        fh = open(filename, 'w')
+        fh.write(document)
+        fh.close()
+
+        self.buffer.set_modified(False)
+        
+    def get_section_by_id(self, id):
+        for section in self:
+            if section.id == id:
+                return section
+        return None
+
+    def get_character_by_id(self, id):
+        for i in self.character:
+            if i.id == id:
+                return i
+        return None
+
+    def get_location_by_id(self, id):  
+        for i in self.location:
+            if i.id == id:
+                return i
+        return None
+
+# [NOTE]
+# This is the old loader. I need it to load the v01 file format in 
 class Loader:
     
     def __init__(self, document):
@@ -355,14 +343,11 @@ class Loader:
             else:
                 if self.first_section:
                     self.first_section = False
-                else:
-                    xml_tag = '</section>\n'
-                    self.document.buffer.insert_at_cursor(xml_tag)
                 cursor_mark = self.document.buffer.get_insert()
                 cursor = self.document.buffer.get_iter_at_mark(cursor_mark)
                 self.document.buffer.create_mark(attrib['id'], cursor, True)
 
-                xml_tag = '<section id="%s">\n' % attrib['id']
+                xml_tag = '<section id="%s" />\n' % attrib['id']
                 self.document.buffer.insert(cursor, xml_tag)
 
         if tag == 'title':
@@ -402,156 +387,26 @@ class Loader:
         if self.stack[-1] == 'content':
             self.document.buffer.insert_at_cursor(text)
 
-class Header:
-
-    def __init__(self):
-        self.title = ''
-        self.author = ''
-        self.language = ''
-        self.summary = ''
-        self.notes = ''
-
-    def to_string(self):
-        # Open tag
-        result = '<head>\n'
-        result += '<title>%s</title>\n' % self.title
-        # Adding optional elements
-        if self.author:
-            result += '<author>%s</author>\n' % self.author
-        if self.language:
-            result += '<language>%s</language>\n' % self.language
-        if self.summary:
-            result += '<summary>\n%s\n</summary>\n' % self.summary
-        if self.notes:
-            result += '<notes>\n%s\n</notes>\n' % self.notes
-        # Close tag
-        result += '</head>\n'
-        return result
-
-class Document(list):
+# The document to migrate v01 to v02
+class Document02(Document):
     
     def __init__(self, page, buffer):
-        list.__init__(self)
-
-        self.page = page
-        self.buffer = buffer
-        self.head = Header()
-        self.character = []
-        self.location = []
-        self.filename = ''
-        self.filepath = ''
+        Document.__init__(self, page, buffer)
 
     def load_file(self, filename):
         assert os.path.isfile(filename)
         self.filepath, self.filename = os.path.split(filename)
-        # [TODO] 
-        # priority: medium
-        # Use the python default xml library.
-        import scanner
+
+        from rayuela import scanner
         fh = open(filename)
         scanner.scan(fh.read(), Loader(self))
-        #.
+        
         fh.close()
-        #.
-    
-    def buffer_to_xml(self):
-        start = self.buffer.get_start_iter()
-        end = self.buffer.get_end_iter()
-        txt = self.buffer.get_text(start, end)
-        # [TODO]
-        # priority: high
-        # Parse the txt, add <body>, <br /> and <p> tags.
-        txt = txt.strip()
-        return state_machine(txt)
-        #lines = txt.split('\n')
-        #n = 0
-        #for i in lines:
-        #    n += 1
-        #    print i
-        #.
-
-    def dump_file(self, filename=''):
-        start = self.buffer.get_start_iter()
-        end = self.buffer.get_end_iter()
-        txt = self.buffer.get_text(start, end)
-
-        if not filename:
-            filename = os.path.join(self.filepath, self.filename)
-        else:
-            self.filepath, self.filename = os.path.split(filename)
-        
-        # Open tag:
-        document = '<rayuela>\n' # lang="%s"> % self.lang
-
-        # sections:
-        sections = "<toc>\n"
-        for section in self:
-            sections += section.to_string()
-        sections += "</toc>\n" 
-
-        document += sections
-        
-        # characters:
-        characters = "<characters>\n"
-        for n in self.character:
-            characters += n.to_string()
-        characters += "</characters>\n" 
-
-        document += characters
-
-        # locations:
-        locations = "<locations>\n"
-        for n in self.location:
-            locations += n.to_string()
-        locations += "</locations>\n" 
-
-        document += locations
-
-        # Add manuscript:
-        manuscript = '<manuscript>\n'
-
-        # Add head:
-        manuscript += self.head.to_string()
-
-        # Add body:
-        body = '<body>\n%s\n</body>\n' % self.buffer_to_xml() #txt.strip()
-        
-        manuscript += body
-        manuscript += '</manuscript>'
-
-        document += manuscript
-
-        # Close tag:
-        document += '</rayuela>'
-
-        fh = open(filename, 'w')
-        fh.write(document)
-        fh.close()
-
-        self.buffer.set_modified(False)
-        
-    def get_section_by_id(self, id):
-        for section in self:
-            if section.id == id:
-                return section
-        return None
-
-    def get_character_by_id(self, id):
-        for i in self.character:
-            if i.id == id:
-                return i
-        return None
-
-    def get_location_by_id(self, id):  
-        for i in self.location:
-            if i.id == id:
-                return i
-        return None
+#.
 
 if __name__ == "__main__":
     import sys
     import gtk
-    d = Document(0, gtk.TextBuffer())
+    d = Document02(0, gtk.TextBuffer())
     d.load_file(sys.argv[1])
     d.dump_file(sys.argv[2])
-    # d.buffer_to_xml()
